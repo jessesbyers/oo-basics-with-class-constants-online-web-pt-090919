dc79dcb9b9c400346b44ea7aa74fbffb84acfdc2
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-19 14:02:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    BRANDS = BRANDS.uniq
   end
 
   def cobble
@@ -14,6 +15,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-BRANDS.uniq
 
 end
